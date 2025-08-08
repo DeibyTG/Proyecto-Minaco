0c686bda5126ba1be9a7440e8b041c91deacfb4f
Deiby Torres <deibytorresg@hotmail.com>
2025-08-08 12:41:09 +0200

    Subimos el proyecto a git
    Subimos el proyecto a git para que el cliente pueda ver el avance del proyecto , dejamos el set interval activado para que pueda ver las animaciones

---
+++ js/ui.js
@@ -0,0 +1,47 @@
+
+//accion carousel de mensaje de bienvenida
+
+
+const contenedorSlider = document.querySelector('.movimiento_carousel');
+const textoSlider=document.querySelectorAll('.mensaje_info_carousel')
+
+let index_carousel_wc=0
+let totalSlides=textoSlider.length
+
+
+
+//vamos a crear una funcion arrow para el movimiento
+//  luego haremos una mas grande para agrupar todo
+
+const primerSlide=textoSlider[0].cloneNode(true);
+contenedorSlider.appendChild(primerSlide);
+
+const totalCon=totalSlides + 1;
+
+function deslizar_mensajes(){
+
+        index_carousel_wc ++;
+        contenedorSlider.style.transition = 'transform 0.6s ease';
+        contenedorSlider.style.transform = `translateX(-${index_carousel_wc * 100}%)`;
+
+        if(index_carousel_wc===totalSlides){
+            setTimeout(()=>{
+                contenedorSlider.style.transition='none'
+                contenedorSlider.style.transform='translateX(0%)';
+                index_carousel_wc=0
+
+                void contenedorSlider.offsetWidth;
+
+                setTimeout(() => {
+                contenedorSlider.style.transition = 'transform 0.6s ease';
+                },20);
+        },600);
+    }
+
+}
+
+setInterval(deslizar_mensajes,3000);
+
+
+
+
