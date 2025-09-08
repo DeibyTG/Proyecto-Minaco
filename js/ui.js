1e8eab21c4acee28f0727f169030c89f7983d383
Deiby Torres <deibytorresg@hotmail.com>
2025-09-08 19:43:45 +0200

    correcticon bug
    Corregimos el bug que hacia que las fotos no se viesen al cargar el carrito

--- js/ui.js
+++ js/ui.js
@@ -3,44 +3,44 @@
 
 
 const contenedorSlider = document.querySelector('.movimiento_carousel');
-const textoSlider=document.querySelectorAll('.mensaje_info_carousel')
+const textoSlider = document.querySelectorAll('.mensaje_info_carousel')
 
-let index_carousel_wc=0
-let totalSlides=textoSlider.length
+let index_carousel_wc = 0
+let totalSlides = textoSlider.length
 
 
 
 //vamos a crear una funcion arrow para el movimiento
 //  luego haremos una mas grande para agrupar todo
 
-const primerSlide=textoSlider[0].cloneNode(true);
+const primerSlide = textoSlider[0].cloneNode(true);
 contenedorSlider.appendChild(primerSlide);
 
-const totalCon=totalSlides + 1;
+const totalCon = totalSlides + 1;
 
-function deslizar_mensajes(){
+function deslizar_mensajes() {
 
-        index_carousel_wc ++;
-        contenedorSlider.style.transition = 'transform 0.6s ease';
-        contenedorSlider.style.transform = `translateX(-${index_carousel_wc * 100}%)`;
+    index_carousel_wc++;
+    contenedorSlider.style.transition = 'transform 0.6s ease';
+    contenedorSlider.style.transform = `translateX(-${index_carousel_wc * 100}%)`;
 
-        if(index_carousel_wc===totalSlides){
-            setTimeout(()=>{
-                contenedorSlider.style.transition='none'
-                contenedorSlider.style.transform='translateX(0%)';
-                index_carousel_wc=0
+    if (index_carousel_wc === totalSlides) {
+        setTimeout(() => {
+            contenedorSlider.style.transition = 'none'
+            contenedorSlider.style.transform = 'translateX(0%)';
+            index_carousel_wc = 0
 
-                void contenedorSlider.offsetWidth;
+            void contenedorSlider.offsetWidth;
 
-                setTimeout(() => {
+            setTimeout(() => {
                 contenedorSlider.style.transition = 'transform 0.6s ease';
-                },20);
-        },600);
+            }, 20);
+        }, 600);
     }
 
 }
 
-setInterval(deslizar_mensajes,3000);
+setInterval(deslizar_mensajes, 3000);
 
 
 
@@ -48,54 +48,73 @@ setInterval(deslizar_mensajes,3000);
 
 //accion para que el main de la pagina index aparezca cuando hagamos scroll hacia abajo
 
-function f_ver_bestSeller(){
-    const mainIndex=document.querySelector('.main-index');
+function f_ver_bestSeller() {
+    const mainIndex = document.querySelector('.main-index');
 
-    if(window.scrollY>100){
+    if (window.scrollY > 100) {
         mainIndex.classList.add('animacion-ver-bs');
-    }else{
+    } else {
         return;
     }
 }
 
 window.addEventListener('scroll', f_ver_bestSeller);
-  
+
 // ahora vamos a hacer un funcion para que cuando hagamos click en uno de los productos nos abra otra pestaña con us informacion precio e imagen 
 
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-    const params=new URLSearchParams(window.location.search);
-    const idProducto=params.get('id');
-
-    fetch('../data/productos.json')
-        .then(res=>res.json())
-        .then(data=>{
-            let producto=null;
-
-            if(data.collares[idProducto]) producto=data.collares[idProducto];
-            else if (data.anillos[idProducto]) producto=data.anillos[idProducto];
-            else if(data.pulsera[idProducto]) producto=data.pulsera[idProducto];
-            else if(data.pendientes[idProducto]) producto=data.pendientes[idProducto];
+    const pageId = document.body.id
+    if (pageId === "producto") {
 
 
-            if(producto){
+        const params = new URLSearchParams(window.location.search);
+        const idProducto = params.get('id');
 
-                const img=document.querySelector('.foto-producto-seleccionado');
-                    img.src=producto.imagen;
-                    img.alt=producto.nombre;
+        fetch('../data/productos.json')
+            .then(res => res.json())
+            .then(data => {
+                let producto = null;
 
-                const info=document.querySelector(".info-producto-seleccionado");
-                    info.innerHTML=`
-                        <h2>${producto.nombre}</h2>
-                        <p>Precio: ${producto.precio}${producto.moneda}</p>
-                    `;
-            }else{
-                document.querySelector(".contenedor-producto-seleccionado").textContent="producto no encontrado"
-            }
+                if (data.collares[idProducto]) producto = data.collares[idProducto];
+                else if (data.anillos[idProducto]) producto = data.anillos[idProducto];
+                else if (data.pulsera[idProducto]) producto = data.pulsera[idProducto];
+                else if (data.pendientes[idProducto]) producto = data.pendientes[idProducto];
 
 
-        });
+                if (producto) {
+
+                    const img = document.querySelector('.foto-producto-seleccionado');
+                    img.src = producto.imagen;
+                    img.alt = producto.nombre;
+
+                    const info = document.querySelector(".info-producto-seleccionado");
+                    info.innerHTML = `
+                            <h2>${producto.nombre}</h2>
+                            <p>Precio: ${producto.precio}${producto.moneda}</p>
+                        `;
+                } else {
+                    document.querySelector(".contenedor-producto-seleccionado").textContent = "producto no encontrado"
+                }
+
+
+            });
+
+    }
+
 });
+
+const botonPedido = document.querySelector(".boton-realizar-pedido");
+if (botonPedido) {
+    botonPedido.addEventListener("click", () => {
+        const params = new URLSearchParams(window.location.search);
+        const idProducto = params.get("id"); // "collar-estrellado"
+        console.log("ID capturada antes de ir al carrito:", idProducto);
+
+        // Redirigir al carrito pasando la misma ID
+        window.location.href = `generar_pedido-carrito.html?id=${idProducto}`;
+    });
+}
 
 
 // Aqui viene el nuevo js pendiente de testear 
@@ -104,53 +123,45 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 // voy a intentar guardar la id de la foto que selecciona el cliente
 
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-    const pageId=document.body.id
+    const pageId = document.body.id
 
-    if(pageId==="carrito"){
+    if (pageId === "carrito") {
 
-        const params=new URLSearchParams(window.location.search);
-        const idProducto=params.get('id');
+        const params = new URLSearchParams(window.location.search);
+        const idProducto = params.get('id');
 
         fetch('../data/productos.json')
-            .then(res=>res.json())
-            .then(data=>{
-                let producto=null;
+            .then(res => res.json())
+            .then(data => {
+                let producto = null;
 
-                if(data.collares[idProducto]) producto=data.collares[idProducto];
-                else if (data.anillos[idProducto]) producto=data.anillos[idProducto];
-                else if(data.pulsera[idProducto]) producto=data.pulsera[idProducto];
-                else if(data.pendientes[idProducto]) producto=data.pendientes[idProducto];
+                if (data.collares[idProducto]) producto = data.collares[idProducto];
+                else if (data.anillos[idProducto]) producto = data.anillos[idProducto];
+                else if (data.pulsera[idProducto]) producto = data.pulsera[idProducto];
+                else if (data.pendientes[idProducto]) producto = data.pendientes[idProducto];
 
 
-                if(producto){
+                if (producto) {
 
-                    const img=document.querySelector('.foto-producto-carrito');
-                        img.src=producto.imagen;
-                        img.alt=producto.nombre;
+                    const img = document.querySelector('.foto-producto-carrito');
+                    img.src = producto.imagen;
+                    img.alt = producto.nombre;
 
-                    const info=document.querySelector(".cantidad-producto-carrito");
-                        info.innerHTML=`
+                    const info = document.querySelector(".cantidad-producto-carrito");
+                    info.innerHTML = `
                             <h2>${producto.nombre}</h2>
                             <p>Precio: ${producto.precio}${producto.moneda}</p>
                         `;
-                }else{
-                    document.querySelector(".cantidad-producto-carrito").textContent="producto no encontrado"
+                } else {
+                    document.querySelector(".cantidad-producto-carrito").textContent = "producto no encontrado"
                 }
 
 
             });
     }
+
 });
 
 
-const botonPedido = document.querySelector(".boton-realizar-pedido");
-botonPedido.addEventListener("click", () => {
-    const params = new URLSearchParams(window.location.search);
-    const idProducto = params.get("id"); // id del producto en esta plantilla
-
-    window.location.href = `generar_pedido-carrito.html?id=${idProducto}`;
-
-    alert(idProducto);
-});
