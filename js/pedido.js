61bcb4143e001b00f41640a2791c7c64a2b8a2c3
Deiby Torres <deibytorresg@hotmail.com>
2025-09-12 11:23:23 +0200

    enviar correo al cliente
    Creamos la opción de que el cliente al hacer un pedido le llegue la confirmacion al correo electronico que puso en el formulario

--- js/pedido.js
+++ js/pedido.js
@@ -14,17 +14,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     form.addEventListener("submit", (e) => {
-        e.preventDefault(); 
+        e.preventDefault(); // evita recarga de página
         console.log("🚀 Submit capturado");
 
-        
+        // Generar un ID único para el pedido
         const idUnico = "PED-" + Date.now();
 
-        
+        // Capturar producto y cantidad desde el HTML del carrito
         const productoNombre = document.querySelector('.cantidad-producto-carrito h2')?.textContent || '';
         const cantidad = document.querySelector('.cantidad-producto-carrito p:last-child')?.textContent.replace('Cantidad: ', '') || '1';
 
-        
+        // Crear objeto con todos los datos a enviar
         const formData = {
             nombre: form.nombre.value,
             apellidos: form.apellidos.value,
@@ -34,9 +34,9 @@ document.addEventListener("DOMContentLoaded", () => {
             comunidad: form.comunidad.value,
             municipio: form.municipio.value,
             codigo_postal: form.codigo_postal.value,
-            nombreProducto: document.getElementById("articuloNombre").innerText,
-            cantidadProducto: document.getElementById("articuloCantidad").innerText,
-            totalPedido: document.getElementById("multiplicador_pedido").innerText,
+            nombreProducto:form.nombreProducto.value,
+            cantidadProducto:form.cantidadProducto.value,
+            totalPedido:form.totalPedido.value,
             id: idUnico,
 
         };
@@ -44,15 +44,20 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("Datos a enviar:", formData);
 
         // Enviar correo con EmailJS
-        emailjs.send("service_qsq7jak", "template_z5necjk", formData)
+        emailjs.send("service_qsq7jak", "template_z5necjk",{...formData,destinatario:"desarrollo.bylot@gmail.com"})
             .then(() => {
-                alert("✅ Pedido enviado con éxito. ID: " + idUnico);
-                form.reset();
+                console.log("✅ Pedido enviado con éxito. ID: " + idUnico);
             })
             .catch(err => {
                 console.error("❌ Error al enviar:", err);
                 alert("❌ Error al enviar: " + JSON.stringify(err));
             });
+
+        emailjs.send("service_qsq7jak", "template_z5necjk",{...formData,destinatario:form.email.value})
+            .then(()=>{
+                alert("Hemos enviado un correo de confirmacion , revisa tu email para comprobar que esta todo correcto")
+                form.reset();
+            })
     });
 
 
